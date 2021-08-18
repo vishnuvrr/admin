3b4948b80422445ab238ffd63eb28055eb968df5
R Vishnu Vardhan Reddy <83265815+vishnuvrr@users.noreply.github.com>
2021-08-18 18:48:42 +0530

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -67,65 +67,6 @@ export default function Sidebar() {
             </div>
         </div>
     </li>
-
-    
-    <hr className="sidebar-divider"/>
-
-    
-    <div className="sidebar-heading">
-        Addons
-    </div>
-
-   
-    <li className="nav-item">
-        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i className="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div className="bg-white py-2 collapse-inner rounded">
-                <h6 className="collapse-header">Login Screens:</h6>
-                <a className="collapse-item" href="login.html">Login</a>
-                <a className="collapse-item" href="register.html">Register</a>
-                <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div className="collapse-divider"></div>
-                <h6 className="collapse-header">Other Pages:</h6>
-                <a className="collapse-item" href="404.html">404 Page</a>
-                <a className="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-   
-    <li className="nav-item">
-        <a className="nav-link" href="charts.html">
-            <i className="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-
-   
-    <li className="nav-item">
-        <a className="nav-link" href="tables.html">
-            <i className="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
-    </li>
-
-    
-    <hr className="sidebar-divider d-none d-md-block"/>
-
-    
-    <div className="text-center d-none d-md-inline">
-        <button className="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
-
-    
-    <div className="sidebar-card d-none d-lg-flex">
-        <img className="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="..."/>
-        <p className="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-        <a className="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-    </div>
-
 </ul>
   )
 }
